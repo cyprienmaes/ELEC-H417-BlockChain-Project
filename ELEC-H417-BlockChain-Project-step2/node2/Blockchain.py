12b06818ac6197af1a2d5c37f638b96f17d41dbf
OddFrogg <45463300+OddFrogg@users.noreply.github.com>
2018-12-19 17:58:24 +0100

    Add files via upload

---
+++ ELEC-H417-BlockChain-Project-step2/node2/Blockchain.py
@@ -0,0 +1,206 @@
+import hashlib
+import json
+from time import gmtime, strftime
+import time
+
+class Blockchain(object):
+#Creattion of the class blockchain which have an empty-list and a list
+#of transaction.
+    def __init__(self):
+        """
+        Constructor of the class
+        """
+        self.chain = [] # initial empty list of the block chain
+        # Blocks wich are comparated to put inside the blockchain
+        # The idea is to have one transaction per block.
+        self.waiting_blocks = [] 
+
+    def create_genesis_block(self,amount):
+        """
+        Create a new genesis block when the block chain is instantiated.
+        """
+        genesis_block = {
+                'index': 0,
+                'timestamp': strftime("%a, %d %b %Y %H:%M:%S", gmtime()),
+                'transactions': amount,
+                'proof' : 100,
+                'time_proof' : 0,
+                'previous_hash': 1
+                 }
+        # Add the hash of the block inside the block
+        genesis_block['hash'] = self.hashBlock(genesis_block)
+        # The block is added to the wainting list of blocks
+        self.putting_block(genesis_block)
+        # Add the new genesis block in the blockchain
+        #self.chain.append(genesis_block)
+        return genesis_block
+                
+                
+
+    def new_block(self, amount, proof, time_proof, previous_hash):
+        """
+        Create a new Block in the Blockchain. This block must follow the
+        previous-hash of the chain.
+        PARAMETERS :
+        - previous_hash : Hash of the previous block
+        - proof :the proof that chain can be sended by the proof of work algorithm
+        RETURN :
+        Block : The new Block
+        """
+        block = {
+                'index': len(self.chain)+1,
+                'timestamp': strftime("%a, %d %b %Y %H:%M:%S", gmtime()),
+                'transaction': amount,
+                'proof' : proof,
+                'time_proof' : time_proof,
+                'previous_hash': previous_hash,
+        }
+
+        # Reset the transaction 
+        self.transactions = 0
+        # Add the hash of the block inside the block
+        block['hash'] = self.hashBlock(block)
+        # We must to make sure that the new block is correct
+        if self.valid_block(block) :
+                # The block is added to the wainting list of blocks
+                self.putting_block(block)
+        else :
+                print("The block " + str(block['index']) + " with an amount of " + str(block['transaction']) + " is not valid")
+        return block
+
+    def choosing_block(self) :
+        """
+        Choice of the block to add in the chain. This choice is made 
+        according to the amount of the highest transaction. If two transactions 
+        are equivalent, the block is chosen according to the smallest proof.
+        RETURN :
+        block_choice : The block added to the chain
+        """
+        # Saving the biggest amount
+        amount_save = 0;
+        # Saving the smallest proof
+        time_proof_save = 30;
+        if len(self.waiting_blocks) >= 1:
+                for block in self.waiting_blocks :
+                        if block['transaction'] > amount_save :
+                                amount_save = block['transaction']
+                                time_proof_save = block['time_proof']
+                                block_choice = block
+                        elif block['transaction'] == amount_save :
+                                if block['time_proof'] < time_proof_save :
+                                        time_proof_save = block['time_proof']
+                                        block_choice = block
+        self.chain.append(block_choice)
+        self.waiting_blocks = []
+        return block_choice
+ 
+    def valid_block(self, block) :
+        """
+        Determine if a given block is valid with the previous-hash and the 
+        hash of the last block inside the chain.
+        PARAMETERS :
+        - Block : the block that we want to make sure it's valid
+        RETURN :
+        - Boolean Expression
+        """
+        if block['previous_hash'] != self.last_block['hash'] :
+                return False
+        else :
+                return True
+
+    
+    def proof_of_work(self):
+        """
+        Proof of work algorithm. Find a number p such the hash(p,previous_proof,
+        previous_hash) contains leading 4 zeroes.
+        PARAMETERS :
+        - last_block : The last block of the chain
+        Return :
+        list of : - proof : The good proof
+                          - t1-t0 : The time to make the proof
+        """
+        last_block = self.last_block()
+        last_proof = last_block['proof']
+        last_hash = last_block['hash']
+        proof = 0
+        t0 = time.clock()
+        while self.valid_proof(last_proof, proof, last_hash) is False:
+                proof += 1
+        t1 = time.clock()
+        return [proof, t1-t0]
+        
+    @staticmethod
+    # The static method doesn't need an object instantiation.
+    def hashBlock(block):
+        """
+        Creates a SHA-256 hash of a Block.
+        PARAMETERS :
+        -Block: A block of the blockchain
+        RETURN :
+        Hash : the hash in 256 bits
+        """
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        # Return in hexadecimal to see the hash with more facilities
+        return hashlib.sha256(block_string).hexdigest()
+        
+    def valid_proof(self, last_proof, proof, last_hash):
+        """
+        Validate the proof of the new block with the proof and the hash 
+        of the last block.
+        PARAMETERS :
+        - last_proof : The previous_proof of the previous_block
+        - proof : The proof that may be validated
+        RETURN : 
+        - Boolean Expression
+        """
+        guess = f'{last_proof}{proof}{last_hash}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+    @property
+    # The property is used to replace getter and setter in our class
+    def last_block(self):
+        """
+        Return the last block of the chain. If the index of the dictionnary 
+        is -1, the block is the end of the blockchain.
+        """
+        return self.chain[-1]
+
+    def putting_block(self,block):
+        """
+        Put a block inside the waiting list of blocks. 
+        PARAMETER :
+        - block : the added block
+        """
+        self.waiting_blocks.append(block)
+
+### --------------------- TEST --------------------- ###
+##new_block_chain = Blockchain()
+##new_block_chain.create_genesis_block(0)
+##
+##lastBlock = new_block_chain.last_block
+##amount = 18
+##t0 = time.clock()
+##new_proof = new_block_chain.proof_of_work(lastBlock)
+##t1 = time.clock()
+##block1 = new_block_chain.new_block(amount, new_proof, t1-t0, lastBlock['hash'])
+##print(block1)
+##lastBlock = new_block_chain.last_block
+##amount = 24
+##t0 = time.clock()
+##new_proof = new_block_chain.proof_of_work(lastBlock)
+##t1 = time.clock()
+##block2 = new_block_chain.new_block(amount, new_proof, t1-t0, lastBlock['hash'])
+##print(block2)
+##lastBlock = new_block_chain.last_block
+##amount = 24
+##t0 = time.clock()
+##new_proof = new_block_chain.proof_of_work(lastBlock)
+##t1 = time.clock()
+##block3 = new_block_chain.new_block(amount, new_proof, t1-t0, lastBlock['hash'])
+##print(block3)
+##new_block_chain.choosing_block()
+##
+##lastBlock = new_block_chain.last_block
+##print(lastBlock)
---
+++ ELEC-H417-BlockChain-Project-step2/node3/node_test.py
@@ -0,0 +1,354 @@
+#!/usr/bin/env python
+
+import socket
+import hashlib
+import ast
+from threading import Thread
+import time
+#import Blockchain
+from Blockchain import Blockchain
+
+
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser
+
+class Node:
+
+    #--------GLOBAL VARIABLES----------------
+
+    #TCP_IP = '127.0.0.1'  # my IP 164.15.244.54
+    TCP_PORT = 5005        # port number used for the TCP authentication center connection
+    TCP_PORT_BROAD = 5004  # testing for broadcoast 
+    BUFFER_SIZE = 1024     # size of the receiveng buffer -- we can adapt it to the lenght
+                           # of our messages witch will speed up the transition
+    #MESSAGE = b"Hello, World!"
+    Password = b'Dricot'   # users password
+    sec = 0                # counter but I think it's gonna be useless
+    data = ''
+
+    #-------------METHODS-------------------
+
+    
+  
+
+        
+    def description(self):
+        return '{} IP is {} and it is connected to the server at {}. Its neighbours are {} and {}' . format(self.username,self.ip_address,self.server_address,self.nextIP1,self.nextIP2)        
+        
+        
+    def runNodesListener(self):
+        """
+        This function runs as a thread. It is responsible for listening to the neighboring nodes
+        """
+        
+        socketNodes = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socketNodes.bind((self.ip_address, 5003))
+
+        while True:
+            socketNodes.listen(1)
+            #print("fuck you")
+            socketNodes.settimeout(1)
+            try :
+                conn, addr = socketNodes.accept()
+                #print(addr)
+                data = conn.recv(self.BUFFER_SIZE)
+                if data:
+                    decriptedData = ast.literal_eval(data.decode('utf-8'))
+                    print(data)
+                    try:
+                        """
+                        We want to know what kind of message we received
+                        Here we consider it is a new block
+                        """
+                        
+                        receivedBlock = decriptedData['Block']
+                        if receivedBlock in self.blockchain.waiting_blocks:
+                            if addr in self.confirmed:
+                                continue
+                            else:
+                                self.confirmed.append(addr)
+                                
+                            if self.verifyConfirmed:
+                                print('coucou')
+                                self.blockchain.chain.append(self.blockchain.waiting_blocks[0])
+                                self.blockchain.waiting_blocks.clear()
+                                self.confirmed.clear()
+                                self.message = b''
+                            else:
+                                pass
+
+                        elif receivedBlock == self.blockchain.last_block() and self.blockchain != []:
+                            pass
+                        
+                        else :
+                            self.blockchain.putting_block(receivedBlock)
+                            self.message = data
+                            nodesMessage = Thread(target = self.runNodesMessage) #Problem. We kill the last thread even if it didn't accomplished the task
+                            nodesMessage.start()
+                    except KeyError:
+                        try:
+                            """
+                            The message is not a new block but a response to a received block
+                            If the block is rejected we drop everything and broadcast a message of rejection
+                            If it is accepted we check if it is accepted by every neighbour if yes we ad it to the chain
+                            and broadcast the info
+
+                            """
+    
+                            receivedConfirmation = decriptedData['Confirmation']
+                            if decriptedData['Conf block'] in blockchain.waiting_blocks:
+                                if receivedConfirmation == 'block rejected':
+                                    self.blockchain.waiting_blocks.clear()
+                                    self.contactedIP.clear()
+                                    self.message = data
+                                    nodesMessage = Thread(target = self.runNodesMessage) #Problem. We kill the last thread even if it didn't accomplished the task
+                                    nodesMessage.start()
+                                elif receivedConfirmation == 'block accepted':
+                                    self.contactedIP[addr] = receivedConfirmation
+                                    if self.verifyIfAccepted():
+                                        self.blockchain.chain.append(self.blockchain.waiting_blocks[0])
+                                        self.blockchain.waiting_blocks.clear()
+                                        self.message = data
+                                        nodesMessage = Thread(target = self.runNodesMessage) #Problem. We kill the last thread even if it didn't accomplished the task
+                                        nodesMessage.start()
+                                        
+                                    else:
+                                        continue
+                                    
+                        except KeyError:
+                            pass
+                else:
+                    continue
+            except socket.timeout:
+                pass
+
+            #print("fuck you")
+
+
+    def runNodesMessage(self):
+        """
+        Function sending infomrmations to other nodes
+        """
+        while True:
+            for neighbour in self.nextIP:
+                socketNodes = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                while True:
+                    try:
+                        print(neighbour)
+                        socketNodes.connect((neighbour, 5003))
+                        socketNodes.send(self.message)
+                        self.contactedIP[neighbour] = 'waiting'
+                        break
+                    except ConnectionRefusedError:
+                        print('error')
+                        pass
+            break
+
+    def setMessage(self,block):
+        message = str(block).encode('utf-8')
+        return message
+
+    def verifyIfAccepted():
+        verified = True
+        for contact in self.nextIP:
+            if 'block accepted' == contactedIP[contact]:
+                verified = True
+            else:
+                verified = False
+                break
+        return verified
+    
+    def verifyConfirmed():
+        verified = True
+        for addr in self.nextIP:
+            
+            if addr in self.confirmed:
+                verified = True
+            else:
+                verified = False
+                break
+        return verified
+            
+            
+        
+
+    
+    def runAuthenticationCenterCom(self):
+        """ fonction run as a thread it's responsible for the communication with
+        the authentication center
+        """
+        
+        while True:
+            answer = input("Write 't' for transaction or 'l' to logout")
+            if answer == 't':
+                authen = self.authenticate()
+                if authen == b'ok':
+                    blockchain = Blockchain()
+                    value = input("Write the value of the transaction: ")
+                    blockchain.new_transaction(value)
+            elif answer == 'l':
+                break
+            else:
+                print('Wrong command')
+                pass
+
+    def usersAction(answer):
+        if answer == 't':
+            authen = self.authenticate()
+            if authen == b'ok':
+                blockchain = Blockchain()
+                value = input("Write the value of the transaction: ")
+                blockchain.new_transaction(value)
+##        elif answer == 'l':
+##            
+##        else:
+##            print('Wrong command')
+##            pass
+
+
+    def authenticate(self):
+        """
+        Fonction which authenticates the user at the authentication center
+        """
+
+        
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.server_address, self.TCP_PORT))
+        message = str({'Username':self.username,'Request': 'send nonce motherfucker'}).encode('utf-8') # User's request of nonce
+        s.send(message)
+        data = s.recv(self.BUFFER_SIZE)
+        hashedMessage = hashlib.sha256()
+        hashedMessage.update(data)
+        hashedMessage.update(self.Password)  # The received nonce and the password are concatenated and hashed
+        myHash = hashedMessage.digest()  #or hexdigest for a more condensed form
+        s.send(myHash)
+        data = s.recv(self.BUFFER_SIZE)
+        print ("received authentication:", data) # Response of the authentication center
+        s.close()
+        return data
+
+
+   
+
+    def sendMessage(self, message):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print(self.server_address, self.TCP_PORT)
+        s.connect((self.server_address, self.TCP_PORT))
+        s.send(message)
+        s.close()
+
+
+
+    def __init__(self):
+        """
+        Constructor of the node
+        """
+        
+        config=ConfigParser()
+        config.read('settings.ini')
+        self.ip_address=config.get('node','ip_address')
+        self.username=config.get('node','username')
+        self.server_address=config.get('registration','ip_address')
+        items = config.items('neigbours')
+        self.nextIP = []   # list of the neighbours' IP addresses
+        i = 0
+        for neighbour in items:
+            self.nextIP.append(neighbour[1])
+            i+=1
+        self.message = b''
+        self.blockchain = Blockchain()
+        self.contactedIP = {}
+        self.confirmed = []
+
+
+def main():
+
+    node = Node()   
+    #MESSAGE = str({'Username':node.username,'Password':node.Password}).encode('utf-8')
+    #node.sendMessage(MESSAGE)
+
+    #blockchain = Blockchain()
+
+    #authenticationCenterCom = Thread(target = self.runAuthenticationCenterCom)
+    
+    nodeListener = Thread(target = node.runNodesListener)        
+    #nodesMessage = Thread(target = node.runNodesMessage)
+    #authenticationCenterCom.setDaemon(True)
+    #nodeListener.setDaemon(True)
+    #nodesMessage.setDaemon(True)
+    #authenticationCenterCom.start()
+    
+    
+    nodeListener.start()
+    #nodesMessage.start()
+    #print('ok')
+    #timer.start()
+    tran_op = 0
+
+##    while True:
+##        while (tran_op==0):
+##            print("Do you want to make a transaction?")
+##            transac_status = input("")
+##            if transac_status =="yes":
+##                #athentication = node.authenticate()
+##                #if authenticate == b'ok'
+##                print("How much money do you want to transfer?")
+##                amount = input("")
+##                if amount.isdigit():                
+##                    if amount == 0:
+##                        tran_op = 1
+##                        print("Transaction Impossible - Amount Null")
+##                        print(node.blockchain.last_block)
+##                        
+##                    else:                                       # insert proof of work here
+##                        #blockchain.new_transaction(amount)
+##                        """
+##                        Here we create a new block that we broadcoast.
+##                        """
+##                        
+##                        
+##                        if (node.blockchain.chain == []):                  # ïf blockchain is empty, create genesis block
+##                            #blockchain.create_genesis_block()
+##                            nodesMessage = Thread(target = node.runNodesMessage) #Problem. We kill the last thread even if it didn't accomplished the task
+##                            node.message = node.setMessage(node.blockchain.create_genesis_block(amount))
+##                            nodesMessage.start()
+##                        else:
+##
+####                          proof, time_proof = node.blockchain.proof_of_work(node.blockchain.last_block())
+####                          previous_hash = hashBlock(node.blockchain.last_block())
+##                            proof, time_proof = node.blockchain.proof_of_work()
+##                            previous_hash = hashBlock(node.blockchain.last_block())
+##                            #blockchain.new_block(blockchain.last_block['hash'])
+##                            node.blockchain.new_block(amount, proof, time_proof, previous_hash)
+##                            newBlock = node.blockchain.new_block(amount, proof, time_proof, previous_hash)
+##                            nodesMessage = Thread(target = node.runNodesMessage) #Problem. We kill the last thread even if it didn't accomplished the task
+##                            node.message = node.setMessage({'Block':newBlock})
+##                            nodesMessage.start()
+##                        print("Transaction Validated")   
+####                        timer.setTime(0)
+####                        while (timer.getTime() < 30):
+####                            if False:                                 # insert block incoming condition here
+####                                blockchain.chain[-1] = []             # deletes created block if it recieves another block
+####                        """append block"""
+##                    
+##                else:
+##                    tran_op = 1;
+##                    print("Transaction Impossible - Wrong Input")
+##            elif transac_status =="no":
+##                print("Ending Transaction")
+##                tran_op = 1
+##            else:
+##                print("Transaction Impossible - Wrong Input")
+##                tran_op = 1
+##        tran_op = 0    
+    
+    
+    
+if __name__ == '__main__': main()
+
+
+
+
+        
